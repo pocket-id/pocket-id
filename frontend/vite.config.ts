a029e92f773673285afa98d9a877150427075727
Jonas Claes <jonas@jonasclaes.be>
2025-03-16 18:45:03 +0100

    feat: add inlang configuration and support for multilingual messages

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,16 @@
+import { paraglideVitePlugin } from '@inlang/paraglide-js';
 import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit(), tailwindcss()]
+	plugins: [
+		paraglideVitePlugin({
+			project: './project.inlang',
+			outdir: './src/lib/paraglide',
+			strategy: ['localStorage', 'cookie', 'baseLocale']
+		}),
+		sveltekit(),
+		tailwindcss()
+	]
 });
