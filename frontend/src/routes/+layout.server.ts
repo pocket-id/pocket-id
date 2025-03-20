110760674d2b5c08488cbb14130883a4c4f0e7bd
Elias Schneider <login@eliasschneider.com>
2025-03-20 16:55:37 +0100

    remove log translation

--- frontend/src/routes/+layout.server.ts
+++ frontend/src/routes/+layout.server.ts
@@ -1,5 +1,4 @@
 import { ACCESS_TOKEN_COOKIE_NAME } from '$lib/constants';
-import { m } from '$lib/paraglide/messages';
 import AppConfigService from '$lib/services/app-config-service';
 import UserService from '$lib/services/user-service';
 import type { LayoutServerLoad } from './$types';
@@ -18,7 +17,7 @@ export const load: LayoutServerLoad = async ({ cookies }) => {
 		.then((config) => config)
 		.catch((e) => {
 			console.error(
-				m.failed_to_get_application_configuration({ message: e.response?.data.error || e.message })
+				`Failed to get application configuration: ${e.response?.data.error || e.message}`
 			);
 			return null;
 		});
