83c37859e985d4ee713b56bf373abb70a913ba63
ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>
2025-03-23 13:50:08 -0700

    fix: hash the refresh token in the DB
    Fixup from #325
    
    Makes sure that refresh tokens are stored as hashes in the database for security, as they are comparable to passwords.
    
    Since refresh tokens are random strings of 40 characters (alphanumeric), they have enough entropy and a short-enough lifespan that a simple SHA-256 hash should suffice.

--- backend/internal/utils/hash_util.go
+++ backend/internal/utils/hash_util.go
@@ -2,10 +2,16 @@ package utils
 
 import (
 	"crypto/sha256"
+	"encoding/base64"
 	"encoding/hex"
 )
 
 func CreateSha256Hash(input string) string {
 	hash := sha256.Sum256([]byte(input))
 	return hex.EncodeToString(hash[:])
+}
+
+func CreateSha256HashBase64(input string) string {
+	hash := sha256.Sum256([]byte(input))
+	return base64.RawStdEncoding.EncodeToString(hash[:])
 }
