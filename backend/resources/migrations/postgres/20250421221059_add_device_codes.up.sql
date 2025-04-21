17023b8263a45ee34c26acf9bfcdb701bc1a7d09
Elias Schneider <login@eliasschneider.com>
2025-04-21 18:09:54 +0200

    fix postgres migration

--- backend/resources/migrations/postgres/20250421221059_add_device_codes.up.sql
+++ backend/resources/migrations/postgres/20250421221059_add_device_codes.up.sql
@@ -1,14 +1,14 @@
 CREATE TABLE oidc_device_codes
 (
-    id             TEXT     NOT NULL PRIMARY KEY,
-    created_at     DATETIME,
-    device_code    TEXT     NOT NULL UNIQUE,
-    user_code      TEXT     NOT NULL UNIQUE,
-    scope          TEXT     NOT NULL,
-    expires_at     DATETIME NOT NULL,
-    interval       INTEGER  NOT NULL,
-    last_poll_time DATETIME,
-    is_authorized  BOOLEAN  NOT NULL DEFAULT FALSE,
-    user_id        TEXT REFERENCES users ON DELETE CASCADE,
-    client_id      TEXT     NOT NULL REFERENCES oidc_clients ON DELETE CASCADE
+    id             UUID        NOT NULL PRIMARY KEY,
+    created_at     TIMESTAMPTZ,
+    device_code    TEXT        NOT NULL UNIQUE,
+    user_code      TEXT        NOT NULL UNIQUE,
+    scope          TEXT        NOT NULL,
+    expires_at     TIMESTAMPTZ NOT NULL,
+    interval       INTEGER     NOT NULL,
+    last_poll_time TIMESTAMPTZ,
+    is_authorized  BOOLEAN     NOT NULL DEFAULT FALSE,
+    user_id        UUID REFERENCES users ON DELETE CASCADE,
+    client_id      UUID        NOT NULL REFERENCES oidc_clients ON DELETE CASCADE
 );
