12b15d5e78f66856711ec62935412b8a7d7e359c
ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>
2025-05-29 08:06:27 -0700

    Move back to multiple jobs, but with jitter

--- backend/internal/job/db_cleanup_job.go
+++ backend/internal/job/db_cleanup_job.go
@@ -2,6 +2,7 @@ package job
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"log/slog"
 	"time"
@@ -16,66 +17,87 @@ import (
 func (s *Scheduler) RegisterDbCleanupJobs(ctx context.Context, db *gorm.DB) error {
 	jobs := &DbCleanupJobs{db: db}
 
-	// Run every 24 hours and now
-	return s.registerJob(ctx, "ClearExpiredDatabaseRecords", gocron.DurationJob(24*time.Hour), jobs.clearExpiredRecords, true)
+	// Run every 24 hours (but with some jitter so they don't run at the exact same time), and now
+	def := gocron.DurationRandomJob(24*time.Hour-2*time.Minute, 24*time.Hour+2*time.Minute)
+	return errors.Join(
+		s.registerJob(ctx, "ClearWebauthnSessions", def, jobs.clearWebauthnSessions, true),
+		s.registerJob(ctx, "ClearOneTimeAccessTokens", def, jobs.clearOneTimeAccessTokens, true),
+		s.registerJob(ctx, "ClearOidcAuthorizationCodes", def, jobs.clearOidcAuthorizationCodes, true),
+		s.registerJob(ctx, "ClearOidcRefreshTokens", def, jobs.clearOidcRefreshTokens, true),
+		s.registerJob(ctx, "ClearAuditLogs", def, jobs.clearAuditLogs, true),
+	)
 }
 
 type DbCleanupJobs struct {
 	db *gorm.DB
 }
 
-func (j *DbCleanupJobs) clearExpiredRecords(ctx context.Context) error {
-	return j.db.Transaction(func(tx *gorm.DB) (err error) {
-		// Deletes WebAuthn sessions that have expired
-		err = tx.
-			WithContext(ctx).
-			Delete(&model.WebauthnSession{}, "expires_at < ?", datatype.DateTime(time.Now())).
-			Error
-		if err != nil {
-			return fmt.Errorf("failed to clean expired WebAuthn sessions: %w", err)
-		}
-		slog.InfoContext(ctx, "Cleaned expired WebAuthn sessions", slog.Int64("count", tx.RowsAffected))
+// ClearWebauthnSessions deletes WebAuthn sessions that have expired
+func (j *DbCleanupJobs) clearWebauthnSessions(ctx context.Context) error {
+	st := j.db.
+		WithContext(ctx).
+		Delete(&model.WebauthnSession{}, "expires_at < ?", datatype.DateTime(time.Now()))
+	if st.Error != nil {
+		return fmt.Errorf("failed to clean expired WebAuthn sessions: %w", st.Error)
+	}
 
-		// Deletes one-time access tokens that have expired
-		err = tx.
-			WithContext(ctx).
-			Delete(&model.OneTimeAccessToken{}, "expires_at < ?", datatype.DateTime(time.Now())).
-			Error
-		if err != nil {
-			return fmt.Errorf("failed to clean expired one-time access tokens: %w", err)
-		}
-		slog.InfoContext(ctx, "Cleaned expired one-time access tokens", slog.Int64("count", tx.RowsAffected))
+	slog.InfoContext(ctx, "Cleaned expired WebAuthn sessions", slog.Int64("count", st.RowsAffected))
 
-		// Deletes OIDC authorization codes that have expired
-		err = tx.
-			WithContext(ctx).
-			Delete(&model.OidcAuthorizationCode{}, "expires_at < ?", datatype.DateTime(time.Now())).
-			Error
-		if err != nil {
-			return fmt.Errorf("failed to clean expired OIDC authorization codes: %w", err)
-		}
-		slog.InfoContext(ctx, "Cleaned expired OIDC authorization codes", slog.Int64("count", tx.RowsAffected))
+	return nil
+}
 
-		// Deletes OIDC refresh tokens that have expired
-		err = tx.
-			WithContext(ctx).
-			Delete(&model.OidcRefreshToken{}, "expires_at < ?", datatype.DateTime(time.Now())).
-			Error
-		if err != nil {
-			return fmt.Errorf("failed to clean expired OIDC refresh tokens: %w", err)
-		}
-		slog.InfoContext(ctx, "Cleaned expired OIDC refresh tokens", slog.Int64("count", tx.RowsAffected))
+// ClearOneTimeAccessTokens deletes one-time access tokens that have expired
+func (j *DbCleanupJobs) clearOneTimeAccessTokens(ctx context.Context) error {
+	st := j.db.
+		WithContext(ctx).
+		Delete(&model.OneTimeAccessToken{}, "expires_at < ?", datatype.DateTime(time.Now()))
+	if st.Error != nil {
+		return fmt.Errorf("failed to clean expired one-time access tokens: %w", st.Error)
+	}
 
-		// Deletes audit logs older than 90 days
-		err = tx.
-			WithContext(ctx).
-			Delete(&model.AuditLog{}, "created_at < ?", datatype.DateTime(time.Now().AddDate(0, 0, -90))).
-			Error
-		if err != nil {
-			return fmt.Errorf("failed to delete old audit logs: %w", err)
-		}
-		slog.InfoContext(ctx, "Deleted old audit logs", slog.Int64("count", tx.RowsAffected))
+	slog.InfoContext(ctx, "Cleaned expired one-time access tokens", slog.Int64("count", st.RowsAffected))
 
-		return nil
-	})
+	return nil
+}
+
+// ClearOidcAuthorizationCodes deletes OIDC authorization codes that have expired
+func (j *DbCleanupJobs) clearOidcAuthorizationCodes(ctx context.Context) error {
+	st := j.db.
+		WithContext(ctx).
+		Delete(&model.OidcAuthorizationCode{}, "expires_at < ?", datatype.DateTime(time.Now()))
+	if st.Error != nil {
+		return fmt.Errorf("failed to clean expired OIDC authorization codes: %w", st.Error)
+	}
+
+	slog.InfoContext(ctx, "Cleaned expired OIDC authorization codes", slog.Int64("count", st.RowsAffected))
+
+	return nil
+}
+
+// ClearOidcAuthorizationCodes deletes OIDC authorization codes that have expired
+func (j *DbCleanupJobs) clearOidcRefreshTokens(ctx context.Context) error {
+	st := j.db.
+		WithContext(ctx).
+		Delete(&model.OidcRefreshToken{}, "expires_at < ?", datatype.DateTime(time.Now()))
+	if st.Error != nil {
+		return fmt.Errorf("failed to clean expired OIDC refresh tokens: %w", st.Error)
+	}
+
+	slog.InfoContext(ctx, "Cleaned expired OIDC refresh tokens", slog.Int64("count", st.RowsAffected))
+
+	return nil
+}
+
+// ClearAuditLogs deletes audit logs older than 90 days
+func (j *DbCleanupJobs) clearAuditLogs(ctx context.Context) error {
+	st := j.db.
+		WithContext(ctx).
+		Delete(&model.AuditLog{}, "created_at < ?", datatype.DateTime(time.Now().AddDate(0, 0, -90)))
+	if st.Error != nil {
+		return fmt.Errorf("failed to delete old audit logs: %w", st.Error)
+	}
+
+	slog.InfoContext(ctx, "Deleted old audit logs", slog.Int64("count", st.RowsAffected))
+
+	return nil
 }
