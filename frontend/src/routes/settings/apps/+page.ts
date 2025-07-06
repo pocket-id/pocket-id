2dc29235db8833260cb82cd93725f3c2d35e22a3
Kyle Mendell <ksm@ofkm.us>
2025-07-05 23:53:03 -0500

    add comment about testing data

--- frontend/src/routes/settings/apps/+page.ts
+++ frontend/src/routes/settings/apps/+page.ts
@@ -5,6 +5,7 @@ import type { PageLoad } from './$types';
 export const load: PageLoad = async () => {
     const oidcService = new OIDCService();
 
+    // this is only for testing we need to make routes to get this data by the users
     const appRequestOptions: SearchPaginationSortRequest = {
         sort: {
             column: 'name',
