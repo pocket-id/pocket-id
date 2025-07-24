5304e9e890e1003bca71cc294bf1a849e24f7028
Kyle Mendell <ksm@ofkm.us>
2025-07-24 17:05:57 -0500

    serve manifest.json by prerendering server route

--- frontend/src/lib/utils/logo-util.ts
+++ frontend/src/lib/utils/logo-util.ts
@@ -2,6 +2,6 @@ import { mode } from 'mode-watcher';
 import { cachedApplicationLogo } from '$lib/utils/cached-image-util';
 
 export function getLogoUrl() {
-  const isLightMode = mode.current === 'light';
-  return cachedApplicationLogo.getUrl(isLightMode);
+	const isLightMode = mode.current === 'light';
+	return cachedApplicationLogo.getUrl(isLightMode);
 }
