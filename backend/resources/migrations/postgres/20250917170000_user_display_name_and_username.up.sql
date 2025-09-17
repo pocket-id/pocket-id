6c857cf1c770c7e3cf97f1319b6e524e61667758
Elias Schneider <login@eliasschneider.com>
2025-09-17 17:25:34 +0200

    feat: allow uppercase usernames

--- backend/resources/migrations/postgres/20250917170000_user_display_name_and_username.up.sql
+++ backend/resources/migrations/postgres/20250917170000_user_display_name_and_username.up.sql
@@ -3,4 +3,10 @@ ALTER TABLE users ADD COLUMN display_name TEXT;
 UPDATE users
 SET display_name = trim(coalesce(first_name,'') || ' ' || coalesce(last_name,''));
 
+ALTER TABLE users
+    DROP CONSTRAINT users_username_key;
+
+CREATE UNIQUE INDEX users_username_lower_idx
+    ON users (lower(username));
+
 ALTER TABLE users ALTER COLUMN display_name SET NOT NULL;
