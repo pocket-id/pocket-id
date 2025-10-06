pocket-id/pocket-id

commit ad8a90c839cc79b542b60ae66c7eb9254fa5f3e4

fix: uploading a client logo with an URL fails if folder doesn't exist

backend/internal/service/oidc_service.go
@@ -1938,6 +1938,11 @@ func (s *OidcService) downloadAndSaveLogoFromURL(parentCtx context.Context, tx *
 	}
 
 	imagePath := common.EnvConfig.UploadPath + "/oidc-client-images/" + clientID + "." + ext
+	err = os.MkdirAll(imagePath, os.ModePerm)
+	if err != nil {
+		return err
+	}
+
 	err = utils.SaveFileStream(io.LimitReader(resp.Body, maxLogoSize+1), imagePath)
 	if err != nil {
 		return err

commit c55701964a84fec08f360cef8135ee68d84b9a33

fix: uploading a client logo with an URL fails
fix issue introduced by ad8a90c839cc79b542b60ae66c7eb9254fa5f3e4

backend/internal/service/oidc_service.go
@@ -15,6 +15,7 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"path/filepath"
 	"regexp"
 	"slices"
 	"strings"
@@ -1937,12 +1938,13 @@ func (s *OidcService) downloadAndSaveLogoFromURL(parentCtx context.Context, tx *
 		return &common.FileTypeNotSupportedError{}
 	}
 
-	imagePath := common.EnvConfig.UploadPath + "/oidc-client-images/" + clientID + "." + ext
-	err = os.MkdirAll(imagePath, os.ModePerm)
+	folderPath := filepath.Join(common.EnvConfig.UploadPath, "oidc-client-images")
+	err = os.MkdirAll(folderPath, os.ModePerm)
 	if err != nil {
 		return err
 	}
 
+	imagePath := filepath.Join(folderPath, clientID + "." + ext)
 	err = utils.SaveFileStream(io.LimitReader(resp.Body, maxLogoSize+1), imagePath)
 	if err != nil {
 		return err