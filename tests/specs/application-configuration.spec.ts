7bde960c75d6e2041c4d162a5def045e3416869a
Elias Schneider <login@eliasschneider.com>
2025-10-31 16:37:34 +0100

    fix e2e tests

--- tests/specs/application-configuration.spec.ts
+++ tests/specs/application-configuration.spec.ts
@@ -126,7 +126,9 @@ test('Update application images', async ({ page }) => {
 	await page.getByLabel('Background Image').setInputFiles('assets/clouds.jpg');
 	await page.getByRole('button', { name: 'Save' }).last().click();
 
-	await expect(page.locator('[data-type="success"]')).toHaveText('Images updated successfully');
+	await expect(page.locator('[data-type="success"]')).toHaveText(
+		'Images updated successfully. It may take a few minutes to update.'
+	);
 
 	await page.request
 		.get('/api/application-images/favicon')
