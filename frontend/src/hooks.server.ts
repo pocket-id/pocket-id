788d6541373331810310961bea59195af9ac6a9d
daimond113 <contact@daimond113.com>
2025-11-02 17:59:54 +0100

    fix: replace %lang% placeholder in html lang

---
+++ frontend/src/hooks.server.ts
@@ -0,0 +1,10 @@
+import type { Handle } from '@sveltejs/kit';
+import { paraglideMiddleware } from '$lib/paraglide/server';
+
+export const handle: Handle = ({ event, resolve }) =>
+	paraglideMiddleware(event.request, ({ request: localizedRequest, locale }) => {
+		event.request = localizedRequest;
+		return resolve(event, {
+			transformPageChunk: ({ html }) => html.replace('%lang%', locale)
+		});
+	});
