70c699ffc6563a706370609519422fdb63ce2667
Melvin Snijders <25155102+MelvinSnijders@users.noreply.github.com>
2025-12-14 18:04:06 +0100

    feat: add email logo endpoint to logger skip prefix list

--- backend/internal/bootstrap/router_bootstrap.go
+++ backend/internal/bootstrap/router_bootstrap.go
@@ -198,6 +198,7 @@ func initLogger(r *gin.Engine) {
 		"GET /api/application-images/logo",
 		"GET /api/application-images/background",
 		"GET /api/application-images/favicon",
+		"GET /api/application-images/email",
 		"GET /_app",
 		"GET /fonts",
 		"GET /healthz",
