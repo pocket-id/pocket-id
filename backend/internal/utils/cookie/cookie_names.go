d6a7b503ff4571b1291a55a569add3374f5e2d5b
Elias Schneider <login@eliasschneider.com>
2026-01-03 23:46:44 +0100

    fix: invalid cookie name for email login code device token

--- backend/internal/utils/cookie/cookie_names.go
+++ backend/internal/utils/cookie/cookie_names.go
@@ -8,7 +8,7 @@ import (
 
 var AccessTokenCookieName = "__Host-access_token"
 var SessionIdCookieName = "__Host-session"
-var DeviceTokenCookieName = "__Host-device_token" //nolint:gosec
+var DeviceTokenCookieName = "__Secure-device_token" //nolint:gosec
 
 func init() {
 	if strings.HasPrefix(common.EnvConfig.AppURL, "http://") {
