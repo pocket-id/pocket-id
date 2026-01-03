2f651adf3b4e8d689461da2083c3afcb1eb1d477
Elias Schneider <login@eliasschneider.com>
2026-01-03 14:32:19 +0100

    fix: migration fails if users exist with no email address

--- backend/resources/migrations/postgres/20260102231700_ldap_admin_group_rename.up.sql
+++ backend/resources/migrations/postgres/20260102231700_ldap_admin_group_rename.up.sql
@@ -1,1 +1,8 @@
-UPDATE app_config_variables SET key = 'ldapAdminGroupName' WHERE key = 'ldapAttributeAdminGroup';
+UPDATE app_config_variables
+SET key = 'ldapAdminGroupName'
+WHERE key = 'ldapAttributeAdminGroup'
+  AND NOT EXISTS (
+    SELECT 1
+    FROM app_config_variables
+    WHERE key = 'ldapAdminGroupName'
+);
--- backend/resources/migrations/sqlite/20260102231700_ldap_admin_group_rename.up.sql
+++ backend/resources/migrations/sqlite/20260102231700_ldap_admin_group_rename.up.sql
@@ -1,7 +1,14 @@
 PRAGMA foreign_keys= OFF;
 BEGIN;
 
-UPDATE app_config_variables SET key = 'ldapAdminGroupName' WHERE key = 'ldapAttributeAdminGroup';
+UPDATE app_config_variables
+SET key = 'ldapAdminGroupName'
+WHERE key = 'ldapAttributeAdminGroup'
+  AND NOT EXISTS (
+    SELECT 1
+    FROM app_config_variables
+    WHERE key = 'ldapAdminGroupName'
+);
 
 COMMIT;
 PRAGMA foreign_keys= ON;
