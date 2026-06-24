99ab38dc799d07d4d91863199b1350fb7d06e03e
Elias Schneider <login@eliasschneider.com>
2026-06-24 22:00:49 +0200

    chore: add support for Docker Hub

--- scripts/development/deploy-development-image.sh
+++ scripts/development/deploy-development-image.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker buildx build --push --file docker/Dockerfile --tag ghcr.io/pocket-id/pocket-id:development --platform linux/amd64,linux/arm64 .
+docker buildx build --push --file docker/Dockerfile --tag pocketid/pocket-id:development --tag ghcr.io/pocket-id/pocket-id:development --platform linux/amd64,linux/arm64 .
