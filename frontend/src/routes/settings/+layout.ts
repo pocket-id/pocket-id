534655874d3a1f23310335356f25e663057a02ca
Elias Schneider <login@eliasschneider.com>
2026-08-24 22:21:40 +0200

    run promises at same time

--- frontend/src/routes/settings/+layout.ts
+++ frontend/src/routes/settings/+layout.ts
@@ -8,26 +8,21 @@ export const load: LayoutLoad = async () => {
 	const storageService = new StorageService();
 	const currentVersion = versionService.getCurrentVersion();
 
-	let newestVersion = null;
-	let isUpToDate: boolean;
-	try {
-		newestVersion = await versionService.getNewestVersion();
-		// If newestVersion is empty, it means the check is disabled or failed.
-		// In this case, we assume the version is up to date.
-		isUpToDate = newestVersion === '' || newestVersion === currentVersion;
-	} catch {
-		// If the request fails, assume up-to-date to avoid showing a warning.
-		isUpToDate = true;
-	}
+	const [newestVersion, sqliteStorageWarning] = await Promise.all([
+		versionService.getNewestVersion().catch(() => null),
+		storageService.getSqliteStorageWarning().catch(() => false)
+	]);
+
+	// If newestVersion is empty, it means the check is disabled or failed.
+	// In this case, we assume the version is up to date.
+	const isUpToDate =
+		newestVersion === null || newestVersion === '' || newestVersion === currentVersion;
 
 	const versionInformation: AppVersionInformation = {
 		currentVersion: versionService.getCurrentVersion(),
 		newestVersion,
 		isUpToDate
 	};
-
-	// If the request fails, don't show the warning to avoid a false positive.
-	const sqliteStorageWarning = await storageService.getSqliteStorageWarning().catch(() => false);
 
 	return {
 		versionInformation,
